66d41ff55087960f90d5b0c952d879d4f7ec3681
Rafael Cassiolato <rcassiolato@outlook.com>
2018-01-23 10:20:03 -0200

    Add files via upload

---
+++ Crypt/Crypt/Cryptor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypt
+{
+    static class Cryptor
+    {
+        
+
+        public static void Sim_Crypt(string msg, out string crypt)
+        {
+            string retorno = "";
+            char[] temp = msg.ToCharArray();
+            int[] subCrypt = new int[temp.Length];
+
+            for (int i = 0; i < temp.Length; i++)
+                subCrypt[i] = (Convert.ToInt32(temp[i]) + 10) * 10;
+
+            for (int i = 0; i < subCrypt.Length; i++)
+                retorno += Convert.ToChar(subCrypt[i]).ToString();
+
+            crypt = retorno;
+
+            return;
+        }
+
+
+        public static void Sim_UnCrypt(string msg, out string uncrypt)
+        {
+            string retorno = "";
+            char[] temp = msg.ToCharArray();
+            int[] subCrypt = new int[temp.Length];
+
+
+            for (int i = 0; i < temp.Length; i++)
+                subCrypt[i] = Convert.ToInt32(temp[i]);
+
+            for (int i = 0; i < subCrypt.Length; i++)
+                subCrypt[i] = (subCrypt[i] / 10) - 10;
+
+            for (int i = 0; i < subCrypt.Length; i++)
+                retorno += Convert.ToChar(subCrypt[i]).ToString();
+
+
+            uncrypt = retorno;
+
+            return;
+        }
+
+
+
+    }
+}
